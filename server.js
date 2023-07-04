f7d8fc864289bfcbf112b9c1a0795d9b48d6d862
Minhaj Hameed <minhajham@yahoo.com>
2023-07-04 19:39:54 +0530

    fixed port env.

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
-const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
-
-const indexRoutes = require('./routes/index')
-const authorRoutes = require('./routes/authors')
-const bookRoutes = require('./routes/books')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit:'10mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-
-mongoose.connect(process.env.DATABASE_URL)
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on('error', err => console.error(err))
-db.once('open', () => console.log('connected to mongoose'))
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/', indexRoutes);
-app.use('/authors', authorRoutes);
-app.use('/books', bookRoutes);
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
-app.listen(process.env.PORT || 3000, () => console.log("server connected to 3000"))
+app.listen((process.env.PORT), () => console.log('server started on designated port'))
